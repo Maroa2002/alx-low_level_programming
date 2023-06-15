540649d6971386fe1eba6388f7c97e321f462787
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-06-15 16:48:36 +0300

    solution to task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//string initialization
+	char str[] = ""Programming is like building a multilingual puzzle";
+	puts(str);//writing string to stdout
+
+	return (0);
+}
