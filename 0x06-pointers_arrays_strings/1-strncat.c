b4bde7b403b8e9dd8d11215752f8f47e69724495
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-06-29 09:15:13 +0300

    provided the solution to task 1 and modified task 1 to return a pointer to a string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+  * *_strncat - concatenates two strings using strncat()
+  * @dest: destination
+  * @src: source
+  * @n: nnumber of characters
+  *
+  * Return: pointer to a string
+  **/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
