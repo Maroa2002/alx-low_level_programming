129f71c6e4d0ec66ff7cd2e4b22a8990e56f2a5f
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-06-29 08:56:29 +0300

    provided the solution to task 0 and also created a readme file containing details of the directory

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+  * *_strcat - concatenating two strings
+  * @dest: destination
+  * @src: source
+  *
+  * Return: pointer to a character
+  **/
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+
+	return (dest);
+}
