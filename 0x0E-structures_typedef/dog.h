0818954c31d14784fef7edee451d058f2073e643
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-07-17 19:13:06 +0300

    solution to task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - defines struct dog
+  * @name: dog's name
+  * @age: dog's age
+  * @owner: dog's owner
+  *
+  * Description: a new type struct dog
+  **/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
