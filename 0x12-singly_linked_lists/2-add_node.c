32595db72b58e5b67d95e337b23b64267e8e079e
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-07-27 18:44:59 +0300

    changes to task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -24,7 +24,13 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node->str = strdup(str);
 	new_node->len = strlen(new_node->str);
-	new_node->next = *head;
+
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+	{
+		new_node->next = *head;
+	}
 	*head = new_node;
 
 	return (new_node);
