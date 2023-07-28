56de2bc611b38fdfa4b1575766cdc1bda1e3829f
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-07-28 12:56:49 +0300

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  * before_function - executes before the main function
+  * Return: void
+  **/
+
+void before_function(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
