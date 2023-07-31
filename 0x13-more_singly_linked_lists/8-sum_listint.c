c1ee4563cf243b33d565396d3f3f95930d0b77c7
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-07-31 17:31:00 +0300

    task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * sum_listint - returns sum of all data
+  * @head: ptr to the first node
+  * Return: sum
+  **/
+
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	if (head == NULL)
+		return (0);
+
+	sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
