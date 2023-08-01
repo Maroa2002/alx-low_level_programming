080998fd254062f378173d1a2d6d8e71e3b3f97f
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-08-01 12:07:24 +0300

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -38,18 +38,18 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	size = get_size_list(head);
 
-	if (head == NULL) /* checking for empty list */
+	if (*head == NULL) /* checking for empty list */
 		return (-1);
 
 	if (index > size) /* checking for invalid input */
 		return (-1);
 
 	temp = *head;
-	if (index == 1)
+	if (index == 0)
 	{
 		*head = (*head)->next;
 		free(temp);
-		return(1);
+		return (1);
 	}
 	else
 	{
