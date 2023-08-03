447e375ee170d160050239144fed9af1fb657ca2
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-08-03 14:53:49 +0300

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * clear_bit - sets the value of a bit to 0
+  * @n: pointer to the given integer
+  * @index: given index
+  * Return: 1 or 0 if an error occurs
+  **/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int size = sizeof(unsigned long int) * 8;
+	unsigned long int mask = 1;
+
+	if (size >= index)
+		return (-1);
+
+	/* given index is set to 0 while the rest to 1 */
+	mask = ~(mask << index);
+	*n &= mask;
+
+	return (1);
+}
