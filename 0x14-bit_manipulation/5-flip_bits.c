39a7357e299789a0ecfd8c27bf9c248b6b90e4f8
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-08-03 15:51:53 +0300

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -17,8 +17,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	difference = n ^ m;
 	count = 0;
 
-	while (difference)
+	/* iterarte through each and every bit using >> */
+	while (difference > 0)
 	{
+		/* check if LSB is set to 1 */
 		count += difference & 1;
 		difference >>= 1;
 	}
