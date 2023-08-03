6430d20badf0056bb9e494d0cb609fa755e195d7
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-08-03 14:33:20 +0300

    updtes to task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -25,7 +25,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 }
 
 /**
-  * other method
+  * other method - another way
   * unsigned long int mask;
   * mask = 1UL << index;
   * *n = *n | mask
