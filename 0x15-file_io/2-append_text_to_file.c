5b25d47e5d0ff4d72390f0fca24f5f04d53832fc
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-08-07 20:44:57 +0300

    task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+  * _strlen - find length of string
+  * @str: string
+  * Return: length
+  **/
+
+int _strlen(char *str)
+{
+	int len;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+
+	return (len);
+}
+
+/**
+  * append_text_to_file - append text and only if file exists
+  * @filename: file
+  * @text_content: appends this content into file
+  * Return: 1 on success, -1 on error
+  **/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int n_wrote;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	/* nothing to write */
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+
+	/* write file */
+	n_wrote = write(fd, text_content, _strlen(text_content));
+	if (n_wrote == -1 || n_wrote != _strlen(text_content))
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
